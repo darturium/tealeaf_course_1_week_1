40503e00891d8a19e88016976250207fa7ef2e0a
Darturium <darturium@gmail.com>
2014-03-05 16:23:18 -0500

    Save code changes

--- blackjack.rb
+++ blackjack.rb
@@ -1,4 +1,3 @@
-require "pry"
 
 def populate_deck
   suits = ["clubs", "diamonds", "hearts", "spades"]
@@ -23,12 +22,12 @@ def populate_deck
     deck["ace_#{suit}".to_sym] = 1
   
     (2..10).each do |num|
-      deck["#{numbers[num]}_#{suit}".to_sym] = num
+      deck["#{numbers[num]}_of_#{suit}".to_sym] = num
     end
 
-    deck["jack_#{suit}".to_sym] = 10
-    deck["queen_#{suit}".to_sym] = 10
-    deck["king_#{suit}".to_sym] = 10
+    deck["jack_of_#{suit}".to_sym] = 10
+    deck["queen_of_#{suit}".to_sym] = 10
+    deck["king_of_#{suit}".to_sym] = 10
 
   end
   
@@ -36,23 +35,14 @@ def populate_deck
 
 end
 
-def deal_cards num, deck
-
+def deal_cards(num, deck)
   cards = []
   keys = deck.keys 
   num.times do
     card_number = rand(0...deck.length)
-    # cards["#{keys[card_number]}".to_sym] = deck[keys[card_number]]
     cards << "#{keys[card_number]}".to_sym
-
-  cards = {}
-  keys = deck.keys 
-  num.times do
-    card_number = rand(0...deck.length)
-    cards ["#{keys[card_number]}".to_sym] = deck[keys[card_number]]
-
   end
-  cards
+  return cards
 end
 
 
@@ -68,19 +58,11 @@ def get_value(cards, deck)
   end
   if ace and sum > 21
     sum -= 10
-
-def get_value cards
-  sum = 0
-  cards.each do |card, value| 
-    #unless card == :ace_clubs or card == :ace_diamonds or 
-    # card == :ace_hearts or card == :ace_spades
-    sum += value   
-
   end
-  sum 
+  sum
 end
 
-
+# start game logic ------------------------->
 print "Hi, welcome to this blackjack game. What is your name? "
 name = gets.chomp
 
@@ -93,53 +75,79 @@ while play
   player_cards = deal_cards(2, deck)
   
   puts "\n"
-  puts "The dealer has the following cards:"
-  puts "One card face-down"
+  puts "Dealer has the following cards:"
+  puts "A card facing down"
   puts "#{dealer_cards[1]}"
 
   puts "\n"
   puts "#{name} has the following cards: "
-  player_cards.each {|card| puts card}
-  puts "Your value is: #{get_value(player_cards, deck)}"
+  player_cards.each_with_index {|card, index| puts "#{index + 1}) #{card}"}
+  puts "#{name}'s cards value: #{get_value(player_cards, deck)}"
   puts "\nWhat do you want to do?"
   puts "1) Hit"
   puts "2) Stay"
+  print "Enter your choice: "
 
   action = gets.chomp
-
-  binding.pry
 
   while action != "1" && action != "2"
     print "You need to enter either 1 or 2: "
     action = gets.chomp
   end
 
+
   case action
-  when 1
-    player_cards << deal_cards(1, deck)
+  when "1"
+    deal_cards(1, deck).each {|card| player_cards << card }
 
     dealer_value = get_value(dealer_cards, deck)
     player_value = get_value(player_cards, deck)
 
     puts "\nDealer has the following cards: "
-    dealer_cards.each {|card| puts "#{card}"}
-    puts "Dealer's cards value: #{dealer_value}"
+    dealer_cards.each_with_index {|card, index| puts "#{index + 1}) #{card}"}
+    puts "Dealer's card value: #{dealer_value}"
     puts "\n#{name} now has the folling cards: "
-    player_cards.each {|card| puts "#{card}" }
+    player_cards.each_with_index {|card, index| puts "#{index + 1}) #{card}"}
     puts "#{name}'s cards value: #{player_value}"
 
     if player_value == 21
-      puts "#{name} wins!"
+      puts "\n#{name} wins!"
     elsif player_value > 21
+      puts "\n#{name} looses"
+    elsif player_value < 21
       if player_value > dealer_value
-        puts "#{name} wins!"
+        puts "\n#{name} wins!"
       elsif dealer_value > player_value
-        puts "#{name} looses"
+        puts "\n#{name} looses"
       end
     end
-  when 2
-  
+  when "2"
+    dealer_value = get_value(dealer_cards, deck)
+    player_value = get_value(player_cards, deck)
+
+    puts "\nDealer has the following cards: "
+    dealer_cards.each_with_index {|card, index| puts "#{index + 1}) #{card}"}
+    puts "Dealer's card value: #{dealer_value}"
+    puts "\n#{name} now has the folling cards: "
+    player_cards.each_with_index {|card, index| puts "#{index + 1}) #{card}"}
+    puts "#{name}'s card value: #{player_value}"
+
+    if player_value > 21
+      puts "#\n{name} looses"
+    elsif player_value >= dealer_value 
+      puts "\n#{name} wins!"
+    else
+      puts "\n#{name} looses"
+    end
   end
 
-  play = false
+  print "Play again? (y OR n): "
+  continue = gets.chomp
+
+  while continue != "y" && continue != "n"
+    print "What? Please enter 'y' or 'n': "
+    continue = gets.chomp
+  end
+
+  play = (continue == "y")
 end
